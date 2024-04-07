ada19da375e7e114874bc0d1957cfd136df447a9
Dani Esquilas <desquilas@gmail.com>
2024-04-07 16:53:00 +0200

    Force autosave with cntrl + s

--- resources/js/Components/Note.vue
+++ resources/js/Components/Note.vue
@@ -1,7 +1,7 @@
 <script setup>
 import BlotFormatter from "quill-blot-formatter"
 import { ImageDrop } from "quill-image-drop-module"
-import { ref, watch } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { notify } from "@kyvg/vue3-notification"
 import QuillEditor from '@/Components/Quill/QuillEditor.vue'
 
@@ -14,7 +14,7 @@ const props = defineProps({
     },
 })
 
-const h = ref(window.innerHeight - 65 - 103)
+
 const noteTitle = ref(props.note?.title)
 const noteContent = ref(props.note?.content)
 
@@ -27,41 +27,6 @@ let lastModified = -1
 const autosaveTime = 1500
 let autosaveInterval = null
 
-/* Quill modules */
-
-const modules = ref([
-    {
-        name: 'blobFormatter',
-        module: BlotFormatter
-    },
-    {
-        name: 'imageDrop',
-        module: ImageDrop
-    }/*,
-    {
-        name: 'better-table',
-        module: QuillBetterTable,
-        operationMenu: {
-            items: {
-                unmergeCells: {
-                    text: 'Unmerge Cells',
-                    icon: 'merge_type',
-                    visible: function (cells) {
-                        return cells.length > 1 || cells[0].colSpan > 1 || cells[0].rowSpan > 1
-                    },
-                    onClick: function (cells) {
-                        this.table.unmergeCells(cells)
-                    }
-                }
-            },
-            keyboard: {
-                bindings: QuillBetterTable.keyboardBindings
-            }
-        },
-
-    }*/
-])
-
 watch(() => props.note, (newNote) => {
     noteTitle.value = newNote.title
     noteContent.value = newNote.content
@@ -70,7 +35,7 @@ watch(() => props.note, (newNote) => {
     }
 }, { deep: true })
 
-const save = () => {
+const dispatchAutosave = () => {
 
     lastModified = Date.now()
     if(!autosaveInterval)
@@ -80,28 +45,42 @@ const save = () => {
 
 let autosave = () => {
 
-    if (lastModified !== -1 && Date.now() - lastModified > autosaveTime) {
-        axios
-            .post('/notes/update/' + props.note.id, {
-                title: noteTitle.value,
-                content: noteContent.value,
-            })
-            .then((response) => {
-                notify({
-                    type: 'success',
-                    text: 'Updated',
-                })
+    if (lastModified !== -1 && Date.now() - lastModified > autosaveTime)
+        save();
+}
 
-                emit('update-note', {note: response.data.note})
-                clearInterval(autosaveInterval);
-                autosaveInterval = null
+const save = () => {
 
-            })
-            .catch((error) => {
-                console.log(error)
+    axios
+        .post('/notes/update/' + props.note.id, {
+            title: noteTitle.value,
+            content: noteContent.value,
+        })
+        .then((response) => {
+            notify({
+                type: 'success',
+                text: 'Updated',
             })
 
-        lastModified = -1
+            emit('update-note', {note: response.data.note})
+            clearInterval(autosaveInterval);
+            autosaveInterval = null
+
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    lastModified = -1
+
+}
+
+
+
+let forceSave = (event) => {
+    if(event.key === "s" && event.ctrlKey){
+        event.preventDefault();
+        save();
     }
 }
 
@@ -109,24 +88,9 @@ let autosave = () => {
 <template>
     <div class="h-full">
         <input @keydown="save()" v-model="noteTitle" placeholder="Nueva nota" type="text" class="w-full bg-cblack border-none focus:outline-none focus:border-none focus:ring-0 text-4xl text-white pl-8 py-8">
-        <QuillEditor @keydown="save()"
-                     placeholder="Start typing..."
-                     :toolbar="[
-                            [{ header: [1, 2, 3, false] }],
-                            ['bold', 'italic', 'underline', 'strike'],
-                            ['blockquote', 'code-block'],
-                            [{ header: 1 }, { header: 2 }],
-                            [{ list: 'ordered' }, { list: 'bullet' }],
-                            [{ indent: '-1' }, { indent: '+1' }],
-                            [{ color: [] }, { background: [] }],
-                            [{ align: [] }],
-                            ['link', 'video', 'image'],
-                            [{table: 'TD'}]
-                    ]"
+        <QuillEditor @keydown="dispatchAutosave"
+                     @keydown.ctrl="forceSave"
                     v-model="noteContent"
-                    content-type="html"
-                    theme="snow"
-                    :modules="modules"
         />
     </div>
 </template>

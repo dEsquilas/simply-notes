6334f2be09c7df6d8a982ccfadf549c3abd4993c
Dani Esquilas <desquilas@gmail.com>
2024-04-07 22:00:08 +0200

    Removed unused routes

--- routes/web.php
+++ routes/web.php
@@ -53,9 +53,6 @@ Route::middleware('auth')->group(function () {
         Route::get('/notes/{noteId}', [NoteController::class, 'view'])->name('note.view');
     });
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';

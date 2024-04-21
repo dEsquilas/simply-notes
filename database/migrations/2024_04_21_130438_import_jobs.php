1c150a8f80b91b6172d51b310f9601f987f98072
Dani Esquilas <desquilas@gmail.com>
2024-04-21 23:24:11 +0200

    Create jobs migrations for table queue and NoteImport

---
+++ database/migrations/2024_04_21_130438_import_jobs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('import_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('notebook_id');
+            $table->string('status');
+            $table->string('total_files')->nullable();
+            $table->string('processed_files')->nullable();
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('import_jobs');
+    }
+};

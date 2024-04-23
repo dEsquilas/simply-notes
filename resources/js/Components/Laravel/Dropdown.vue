9d7788b9e6a07d894b15aa2cc390f1f1e3527c93
Dani Esquilas <desquilas@gmail.com>
2024-04-23 20:11:57 +0200

    Fix logout hover

--- resources/js/Components/Laravel/Dropdown.vue
+++ resources/js/Components/Laravel/Dropdown.vue
@@ -68,7 +68,7 @@ const open = ref(false)
                 style="display: none"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5 bg-[var(--cgray)]" :class="contentClasses">
+                <div class="rounded-md ring-1 ring-black ring-opacity-5 bg-cgray" :class="contentClasses">
                     <slot name="content" />
                 </div>
             </div>

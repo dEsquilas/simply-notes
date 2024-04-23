cd2d90d8d2e43d5f1d98b51915597b753e9a40f5
Dani Esquilas <desquilas@gmail.com>
2024-04-23 21:34:08 +0200

    Added DateHelper

---
+++ resources/js/Helpers/DateHelper.js
@@ -0,0 +1,9 @@
+export default {
+    formatDate(date, locale = 'es-ES') {
+        return new Intl.DateTimeFormat(locale, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).format(new Date(date));
+    }
+}

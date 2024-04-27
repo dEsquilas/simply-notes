1482002936bcd35ced44f418321120e153ec1610
Dani Esquilas <desquilas@gmail.com>
2024-04-27 09:44:28 +0200

    Fix login screen on rwd

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="flex h-screen">
-        <div class="w-1/2 flex">
+    <div class="flex h-screen flex-col lg:flex-row">
+        <div class="flex
+                    w-full
+                    h-1/2
+                    md:w-1/2 md:h-full
+            ">
             <div class="w-[300px] m-auto">
                 <img alt="logo" src="/favicon.png" class="mb-8 w-[80px]">
                 <h2 class="text-2xl mb-2 font-bold">Welcome to SimplyNotes</h2>
@@ -8,7 +12,11 @@
                 <slot />
             </div>
         </div>
-        <div class="w-1/2 bg-cover bg-center login-image">
+        <div class="bg-cover bg-center login-image
+                    w-full
+                    h-1/2
+                    md:w-1/2 md:h-full
+                    ">
 
         </div>
     </div>
